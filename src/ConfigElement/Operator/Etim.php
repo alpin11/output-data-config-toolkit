3f0e07c97d0882f26593504e00d318b50eddc24d
Dominik Ager <d.ager@alpin11.at>
2022-02-15 11:47:27 +0100

    Update Etim.php

--- src/ConfigElement/Operator/Etim.php
+++ src/ConfigElement/Operator/Etim.php
@@ -20,15 +20,12 @@ use Pimcore\Model\DataObject\Classificationstore\KeyConfig;
 class Etim extends AbstractOperator
 {
     protected $etimFeatureCode;
-    protected $wtf;
 
     public function __construct($config, $context = null)
     {
         $this->etimFeatureCode = $config->etimFeatureCode;
 
         $this->context = $context;
-
-        $this->wtf = KeyConfig::getByName("ef000001");
     }
 
     public function getLabeledValue($object)
